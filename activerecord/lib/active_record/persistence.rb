b2cb06c3a813f57a07dd5b9b66e1a9fc85b64c23
Matthew Draper <matthew@trebex.net>
2026-06-01 07:33:51 +0000

    Keep reload fully unscoped internally
    Bare `unscoped` now preserves named default scopes, but `reload` uses this
    path as an implementation detail for queries that should ignore default
    scopes entirely.

--- activerecord/lib/active_record/persistence.rb
+++ activerecord/lib/active_record/persistence.rb
@@ -792,7 +792,7 @@ module ActiveRecord
       fresh_object = if apply_scoping?(options)
         _find_record((options || {}).merge(all_queries: true))
       else
-        self.class.unscoped { _find_record(options) }
+        self.class.relation.scoping { _find_record(options) }
       end
 
       @association_cache = fresh_object.instance_variable_get(:@association_cache)
